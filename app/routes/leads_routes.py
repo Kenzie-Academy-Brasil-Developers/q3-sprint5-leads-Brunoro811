73a2b49b3d09b965bbed444008780b59d96100f6
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-02-25 10:15:56 -0300

    feat: add gunicorn

--- app/routes/leads_routes.py
+++ app/routes/leads_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from app.controllers import leads_controllers
 
-bp = Blueprint('leads', __name__, url_prefix="/leads")
+bp = Blueprint("leads", __name__, url_prefix="/leads")
 
 bp.post('')(leads_controllers.create_lead)
 bp.get('')(leads_controllers.get_all_leads)
